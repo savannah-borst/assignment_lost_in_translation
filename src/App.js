f6448ca5c0a4661d82619444c048724285f8a03d
savannah-borst <savannah.borst@gmail.com>
2022-03-20 15:56:00 +0100

    feat: setting up router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.scss';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom'
+import Start from './components/Start.js'
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>React Translation Page</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1>React Translation Page</h1>
+      </div>
+      <Switch>
+        <Route path='/' exact component={ Start }/>
+      </Switch>
+    </BrowserRouter>
+    
   );
 }
 
---
+++ src/components/Start.js
@@ -0,0 +1,7 @@
+function Login() {
+    return(
+        <main className="Login">
+            <h1>Login to Translation</h1>
+        </main>
+    )
+}
