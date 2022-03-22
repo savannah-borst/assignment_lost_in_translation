317c593e909de573c925e0b37b873351dc8aaf78
savannah-borst <savannah.borst@gmail.com>
2022-03-22 16:56:24 +0100

    setting up API and update middleware

--- src/store/middleware/loginMiddleware.js
+++ src/store/middleware/loginMiddleware.js
@@ -1,17 +1,23 @@
-import { ACTION_LOGIN_ATTEMPT, ACTION_LOGIN_FAILED, ACTION_LOGIN_SUCCES } from "../actions/loginActions";
+import { LoginAPI } from "../../components/Start/LoginAPI";
+import { ACTION_LOGIN_ATTEMPT, ACTION_LOGIN_FAILED, ACTION_LOGIN_SUCCES, loginFailedAction, loginSuccesAction } from "../actions/loginActions";
 
 export const loginMiddleware = ({ dispatch }) => next => action => {
     next(action);
 
     if (action.type === ACTION_LOGIN_ATTEMPT) {
         //MAKE HTTP REQUEST
+        LoginAPI.login(action.payload)
+        .then(profile => {
+            //LOGIN SUCCES
+            dispatch(loginSuccesAction(profile))
+        })
+        .catch(error => {
+            dispatch(loginFailedAction(error))
+        })
+
     }
 
     if (action.type === ACTION_LOGIN_SUCCES) {
 
-    }
-
-    if (action.type === ACTION_LOGIN_FAILED) {
-        
     }
 }
