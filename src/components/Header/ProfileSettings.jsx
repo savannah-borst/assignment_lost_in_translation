d2d7192e229a72bd74bb626a2cd7ed3cea2bb771
Richie Schuurman <richie.schuurman@hotmail.com>
2022-03-25 14:16:27 +0100

    Added conditional header part

--- src/components/Header/ProfileSettings.jsx
+++ src/components/Header/ProfileSettings.jsx
@@ -1,13 +1,22 @@
 import PersonIcon from "@mui/icons-material/Person"
+import { useDispatch, useSelector } from 'react-redux';
+import store from '../../store/store';
 
 function ProfileSettings() {
+  const { user } = useSelector(state => state.user)
+  const { loggedIn } = useSelector(state => state.session)
+
   return (
+    <>
+    {loggedIn &&
     <div className="profile-settings">
-      Richie
+      {user.username}
       <span className="profile-settings-icon">
         <PersonIcon />
       </span>
     </div>
+    }
+    </>
   );
 }
 
